5b7765de2b49e825056c0d1b45315c1a04cd05b1
Harrison <hrouillard@sfi.com.au>
2024-04-09 22:27:22 +1000

    print formatting

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
         let file_path = &args[2];
         let torrent = parse_file(file_path.to_string());
         println!(
-            "Tracker URL: {}, Length: {}",
+            "Tracker URL: {}, Length: {:?}",
             torrent.announce, torrent.info.length.to_string()
         )
     } else {
